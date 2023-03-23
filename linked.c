a8d1589648a7c12762ce9ddb32f15a2be0eeb027
Gagan Sharma <80513417+DemiwolfLegend@users.noreply.github.com>
2023-03-23 17:21:18 +0530

    adding append function

--- linked.c
+++ linked.c
@@ -13,11 +13,25 @@ int count(struct node *);
 
 void display(struct node *);
 
+void append(struct node **, int);
+
 // void main function
 void main()
 {
     struct node *p;
     p = NULL;
+
+    printf("\nNo. of elements in the linked list = %d", count(p));
+    append(&p, 1);
+    append(&p, 2);
+    append(&p, 3);
+    append(&p, 4);
+    append(&p, 17);
+    append(&p, 18);
+    append(&p, 19);
+    printf("\nNo. of elements in the linked list = %d", count(p));
+
+    display(p);
 }
 
 int count(struct node *q)
@@ -39,5 +53,33 @@ void display(struct node *q)
     {
         printf("%d  ", q->value);
         q = q->link;
+    }
+}
+
+void append(struct node **q, int num)
+{
+    struct node *temp, *r;
+    temp = *q;
+
+    if (*q == NULL)
+    {
+        temp = malloc(sizeof(struct node));
+        temp->value = num;
+        temp->link = NULL;
+        *q = temp;
+    }
+    else
+    {
+        temp = *q;
+
+        while (temp->link != NULL)
+        {
+            temp = temp->link;
+        }
+
+        r = malloc(sizeof(struct node));
+        r->value = num;
+        r->link = NULL;
+        temp->link = r;
     }
 }
